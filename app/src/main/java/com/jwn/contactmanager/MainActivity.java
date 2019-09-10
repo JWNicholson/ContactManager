d9b04ac2e932a4f82afdf3c9dcab732430251c48
JWNicholson <john.nicholson812@gmail.com>
2019-09-10 14:29:47 -0400

    working using recycler view, and clicking contact opens a different view displaying that contacts info

--- app/src/main/java/com/jwn/contactmanager/MainActivity.java
+++ app/src/main/java/com/jwn/contactmanager/MainActivity.java
@@ -1,34 +1,57 @@
 package com.jwn.contactmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 
+import com.jwn.contactmanager.adapter.RecyclerViewAdapter;
 import com.jwn.contactmanager.data.DatabaseHandler;
 import com.jwn.contactmanager.model.Contact;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerViewAdapter recyclerViewAdapter;
+    private ArrayList<Contact> contactArrayList;
+    private ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+        contactArrayList = new ArrayList<>();
         DatabaseHandler db = new DatabaseHandler(MainActivity.this);
-
-
-Log.d("Count","onCreate: " + db.getCount());
-
         List<Contact> contactList = db.getAllContacts();
 
 
         for (Contact contact : contactList) {
             Log.d("MainActivity", "onCreate: " + contact.getName());
+            contactArrayList.add(contact);
 
         }
+
+        //setup adapter
+        recyclerViewAdapter = new RecyclerViewAdapter(MainActivity.this, contactArrayList);
+
+        recyclerView.setAdapter(recyclerViewAdapter);
+
+
+
+
+
+
 
     }//end onCreate
 }//end MainActivity
